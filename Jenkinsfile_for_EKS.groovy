5002780c394759c8baaeab7b8527e3f81f1a31f5
Mahsuda <maqsudat@yahoo.com>
2020-02-08 13:22:49 -0600

    added

--- Jenkinsfile_for_EKS.groovy
+++ Jenkinsfile_for_EKS.groovy
@@ -3,10 +3,13 @@ node {
         git 'https://github.com/bkarakas/terraform-iaac-eks-burak'
     }
     stage("stage1"){
-        echo "Hello"
+      sh "Terraform_version"
+      sh "wget https://releases.hashicorp.com/terraform/0.12.20/terraform_0.12.20_linux_amd64.zip"
+      sh "unzip terraform_0.12.20_linux_amd64.zip"
+      sh  "./terraform version"
     }
     stage("stage1"){
-        echo "Hello"
+        sh "source setenv.sh configuration/dev/us-west-2/dev.tfvars"
     }
     stage("stage1"){
         echo "Hello"
