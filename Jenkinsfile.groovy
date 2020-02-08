85abc9d58fd82a155f0a1365633f36da36f1889d
Mahsuda <maqsudat@yahoo.com>
2020-02-08 10:47:11 -0600

    added property

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,5 @@
 node {
-properties([parameters([choice(choices: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'], description: 'Please select a region', name: '')])])
+    properties([parameters([choice(choices: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'], description: 'Please select a region', name: '')])])
     
     stage("Pull Repo"){
         echo "Hello"
