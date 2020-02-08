da64b94a158eccd85f98195fc3c13404767dc551
Mahsuda <maqsudat@yahoo.com>
2020-02-08 13:43:10 -0600

    added

--- Jenkinsfile_for_EKS.groovy
+++ Jenkinsfile_for_EKS.groovy
@@ -1,17 +1,25 @@
-node {
-    stage("Pull repo"){
-        git 'git@github.com:Mahsuda/terraform-iaac-eks-burak.git'
+node{
+    stage("Pull Repo"){
+        ws ("tmp/"){
+            git 'https://github.com/farrukh90/terraform-iaac-eks-burak.git'
+        }
+        
     }
-    stage("stage1"){
-      sh "Terraform_version"
-      sh "wget https://releases.hashicorp.com/terraform/0.12.20/terraform_0.12.20_linux_amd64.zip"
-      sh "unzip terraform_0.12.20_linux_amd64.zip"
-      sh  "./terraform version"
+    stage("Download Terraform"){
+        ws ("tmp/") {
+            sh "terraform version"
+            sh "wget https://releases.hashicorp.com/terraform/0.12.19/terraform_0.12.19_linux_amd64.zip"
+            sh "unzip -o terraform_0.12.19_linux_amd64.zip"
+            sh "./terraform version"}
     }
-    stage("stage1"){
-        sh "source setenv.sh configuration/dev/us-west-2/dev.tfvars"
+    stage("Set Backend"){
+        ws ("tmp/"){
+            sh "./terraform init"
+        }
     }
-    stage("stage1"){
-        echo "Hello"
+    stage("Plan"){
+        ws ("tmp/") {
+            sh "./terraform plan -var-file configurations/dev/us-west-2/dev.tfvars"
+        }
     }
 }
