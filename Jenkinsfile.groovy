4225a0a6203ae872a8d56b3eefbacc4be6c1c1bd
Mahsuda <maqsudat@yahoo.com>
2020-02-08 11:01:25 -0600

    added region to jenkins

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,12 +2,13 @@ node {
     properties([parameters([choice(choices: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'], description: 'Please select a region', name: '')])])
     
     stage("Pull Repo"){
-        echo "Hello"
+        git 'https://github.com/Mahsuda/packer.git'
 
     }
 
     stage("Build an image"){
         sh "packer version"
+        sh "packer build -var region=${AMI_REGION} tools/jenkins_example.json"
 
     }
 
