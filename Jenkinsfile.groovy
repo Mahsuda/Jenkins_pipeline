820d49a8f3c57810cc4225c6c94ab84b9e6dbc88
Mahsuda <maqsudat@yahoo.com>
2020-02-08 11:20:45 -0600

    added region

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,5 @@
 node {
-    properties([parameters([choice(choices: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'], description: 'Please select a region', name: '')])])
+    properties([parameters([choice(choices: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'], description: 'Please select a region', name: 'AMI_REGION')])])
     
     stage("Pull Repo"){
         git 'https://github.com/Mahsuda/packer.git'
@@ -7,11 +7,11 @@ node {
 
     stage("Build an image"){
         sh "packer version"
-        sh "packer build -var region=${AMI_REGION} tools/jenkins_example.json"
+        //sh "packer build -var region=${AMI_REGION} tools/jenkins_example.json"
         }
 
     stage("Send notification to slack"){
-        echo "Hello"
+        slackSend channel: 'nagios_alert ', message: 'Image_built'
         }
 
     stage("Send email"){
