62b353cc53d3d625dd60a280ba9916b7dd24f2ac
Mahsuda <maqsudat@yahoo.com>
2020-02-08 10:33:12 -0600

    jenkins packer image added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,1 +1,20 @@
+node {
+    stage("Pull Repo"){
+        echo "Hello"
 
+}
+
+    stage("Build an image"){
+        sh "packer version"
+
+}
+
+    stage("Send notification to slack"){
+        echo "Hello"
+
+}
+
+    stage("Send email"){
+        echo "Hello"
+
+}
